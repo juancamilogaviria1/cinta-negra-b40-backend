ab2d12950e2d7fc6a23e4bfe840f142696c06525
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-18 16:13:32 -0500

    refactorizar comparar contraseñas

--- models/Users.js
+++ models/Users.js
@@ -50,8 +50,8 @@ bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
 
 UserSchema.methods.comparePassword = function(candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-        if (err) return cb(err);
-        cb(null, isMatch);
+        if (err) return false;
+        return isMatch;
     });
 };
 
