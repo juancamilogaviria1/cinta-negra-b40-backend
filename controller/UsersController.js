08f8d680b6622c6f91f1f4c536bc3252e6221b05
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-18 15:09:59 -0500

    Agregar endpoint para signup

--- controller/UsersController.js
+++ controller/UsersController.js
@@ -3,7 +3,7 @@ const {UsersService} = require('../services');
 module.exports = {
     create: async (req, res) => {
         try {     
-        const user = await  UsersService.create(req, body);
+        const user = await  UsersService.create(req.body);
         res.status(201).send(user)
         } catch (err) {
         res.status(400).send({ message: 'Error creating user', err });
@@ -47,6 +47,15 @@ module.exports = {
         } catch(err){
             res.status(404).send({ message: 'Error deleting user', err });
         } 
+    },
+    signup: async (req, res) => {
+        try {     
+        const user = await  UsersService.create(req.body);
+        res.status(201).send({ message:"signup succesfull", user});
+        } catch (err) {
+        res.status(400).send({ message: 'Error signin up', err });
+        }
+
     },
 
 }
--- router/UsersRoutes.js
+++ router/UsersRoutes.js
@@ -3,22 +3,19 @@ const router = express.Router();
 const { UsersController } = require ('../controller');
 
 //CREATE USER
-
 router.post('/users', UsersController.create);
-
 //GET ALL
-
 router.get('/users', UsersController.find);
-
 //GET ONE
-
 router.get('/users/:id', UsersController.findById);
-
-//DELETE
+//UPDATE
 router.patch('/users/:id', UsersController.findByIdAndUpdate);
-
 //DELETE
 router.delete('/users/:id', UsersController.findByIdAndDelete);
+
+//LOGIN
+router.post('/users/signup', UsersController.signup);
+// router.post('/users/login', UsersController.login);
 
 
 module.exports = router;
