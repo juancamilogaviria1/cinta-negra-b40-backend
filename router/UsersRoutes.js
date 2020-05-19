3cfdc7669f4099c73514ce52bc702fec45190bb0
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-18 20:49:00 -0500

    Agregar verificacion de token para proteger las rutas

--- router/UsersRoutes.js
+++ router/UsersRoutes.js
@@ -2,10 +2,24 @@ const express = require('express');
 const router = express.Router();
 const { UsersController } = require ('../controller');
 
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    try {
+        const authorization = req.headers;
+        const token = authorization.split(" ")[1];
+        const decode = jwt.verify(token, proces.env.JWT_SECRET);
+        req.decode = decode;
+        next();
+    } catch (err) {
+        res.status(403).send({ message: "Error with token", err })
+    }
+};
+
 //CREATE USER
 router.post('/users', UsersController.create);
 //GET ALL
-router.get('/users', UsersController.find);
+router.get('/users', verifyToken, UsersController.find);
 //GET ONE
 router.get('/users/:id', UsersController.findById);
 //UPDATE
