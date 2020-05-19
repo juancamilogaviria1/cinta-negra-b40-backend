f5afaa4477c1bf530f4c21919798c5e246850090
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-19 18:40:00 -0500

    Se agrega validación en index de middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = {
     verifyToken: (req, res, next) => {
         try {
+            if (!req.headers.authorization) res.status(403).send({ error: "Authorization header must be provided" });
             const { authorization } = req.headers;
             const token = authorization.split(" ")[1];
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
