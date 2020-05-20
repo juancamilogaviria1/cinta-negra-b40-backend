500bffd0d8653ebb626967049f542b734b76fde4
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-20 18:54:06 -0500

    Agregamos rutas de Roles

--- router/PrivateRoutes.js
+++ router/PrivateRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 
 
 router.use(require('./UsersRoutes'));
+router.use(require('./RolesRoutes'));
+
 
 module.exports = router;
---
+++ router/RolesRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { RolesController } = require('../controller');
+const { RolesValidator } = require('../validators');
+
+//CREATE USER
+router.post('/users/:id/roles', RolesValidator.create, RolesController.create);
+// //GET ALL
+// router.get('/users/:id/roles',  RolesController.find);
+// //UPDATE
+// router.patch('/users/:id/roles/:idRole', RolesValidator.update, RolesController.findByIdAndUpdate);
+// //DELETE
+// router.delete('/users/:id/roles/:idRole', RolesController.findByIdAndDelete);
+
+
+module.exports = router;
