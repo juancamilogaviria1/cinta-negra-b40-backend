6cfa4f1c1b121b3a2597a7ab0d989b3facf73ed2
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-20 18:57:31 -0500

    Observacion sobre el token

--- router/index.js
+++ router/index.js
@@ -4,7 +4,12 @@ const { verifyToken } = require('../middlewares')
 
 router.use(require('./PublicRoutes'));
 
-router.use(verifyToken);
+
+// Al comentar nos permite realizar peticion sin el token, de lo contrario
+// hay que hacer peticion a login para tomar el token. 
+
+// router.use(verifyToken); 
+
 router.use(require('./PrivateRoutes'));
 
 module.exports = router;
