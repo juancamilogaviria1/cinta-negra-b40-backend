2343b59af10fb8f0475203ad795c4816f5d7680a
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-21 18:27:37 -0500

    comentar linea

--- router/SensorsRoutes.js
+++ router/SensorsRoutes.js
@@ -16,10 +16,10 @@ router.delete('/sensors/:id', SensorsController.findByIdAndDelete);
 
 
 
-router.post('/sensors/:id/sensors', SensorsValidator.addSensorToUser,  SensorsController.addSensorToUser);
-router.get('/sensors/:id/sensors',  SensorsController.findUserSensors);
-router.get('/sensors/:idUser/sensors/:idSensor', SensorsController.findUserSensorById);
-router.delete('/sensors/:idUser/sensors/:idSensor', SensorsController.deleteUserSensorById);
+// router.post('/sensors/:id/sensors', SensorsValidator.addSensorToUser,  SensorsController.addSensorToUser);
+// router.get('/sensors/:id/sensors',  SensorsController.findUserSensors);
+// router.get('/sensors/:idUser/sensors/:idSensor', SensorsController.findUserSensorById);
+// router.delete('/sensors/:idUser/sensors/:idSensor', SensorsController.deleteUserSensorById);
 
 
 module.exports = router;
