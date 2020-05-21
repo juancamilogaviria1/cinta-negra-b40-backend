51ba5449dbbaa484d1e18e8964acccae769573d6
juancamilogaviria1 <juancamilogaviria1@gmail.com>
2020-05-21 15:09:39 -0500

    Nuevas rutas de Sensores

--- router/SensorsRoutes.js
+++ router/SensorsRoutes.js
@@ -15,5 +15,12 @@ router.patch('/sensors/:id', SensorsController.findByIdAndUpdate);
 router.delete('/sensors/:id', SensorsController.findByIdAndDelete);
 
 
+
+router.post('/sensors/:id/sensors', SensorsValidator.addSensorToUser,  SensorsController.addSensorToUser);
+router.get('/sensors/:id/sensors',  SensorsController.findUserSensors);
+router.get('/sensors/:idUser/sensors/:idSensor', SensorsController.findUserSensorById);
+router.delete('/sensors/:idUser/sensors/:idSensor', SensorsController.deleteUserSensorById);
+
+
 module.exports = router;
 
